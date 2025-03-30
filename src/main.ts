ace16aa2d43c642b5ee4779dcefa49d802c82058
Mohammed Maher <mohamed.maher@digi360.tech>
2025-03-30 15:04:03 +0200

    feat: Use default port 3000 and remove PORT variable from railway.json
    - Changed the default port in `src/main.ts` to 3000.
    - Removed the `PORT` variable from `railway.json`. This allows Railway to assign the port automatically.

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   });
 
   const configService = app.get(ConfigService);
-  const port = configService.get<number>('PORT') || 3001;
+  const port = configService.get<number>('PORT') || 3000;
 
   app.enableCors();
 
